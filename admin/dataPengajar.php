353763b6bbe7d68fedb92a86764f5b21e75ac5bc
isra <isramiraza004@gmail.com>
2023-05-15 11:38:47 +0700

    revisi main

--- admin/dataPengajar.php
+++ admin/dataPengajar.php
@@ -2,4 +2,25 @@
     <div>
         <h3>Data Pengajar</h3>
     </div>
+    <div class="overflow-x-auto mt-[50px]">
+        <table class="table w-full">
+            <!-- head -->
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Kode Guru</th>
+                    <th>Nama</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>1</th>
+                    <td>Cy Ganderton</td>
+                    <td>Quality Control Specialist</td>
+                    <td>Blue</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </section>
